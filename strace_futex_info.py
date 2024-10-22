cf18ff749073dbdac8a563cb6ae767cd7dfbd5b9
Patrick Ladd <pladd@redhat.com>
2024-10-22 10:52:55 -0400

    Add timed/untimed wait counts

--- strace_futex_info.py
+++ strace_futex_info.py
@@ -9,8 +9,12 @@ call_count = 0
 return_count = 0
 linenum = 0
 
+timed_wait = 0
+untimed_wait = 0
+
 with open(sys.argv[1]) as stracef:
     for line in stracef:
+        linenum += 1
         if 'resumed' not in line and 'killed by' not in line and 'exited with' not in line and '--- SIG' not in line:
             call = line.split()[2].split('(')[0]
 
@@ -18,32 +22,26 @@ with open(sys.argv[1]) as stracef:
                 continue
 
             call_count += 1
-            futex_op = line.split()[3]
+            futex_op = line.split()[3][:-1]
             if futex_op in ops:
                 ops[futex_op] = ops[futex_op] + 1
             else:
                 ops[futex_op] = 1
+
+            if futex_op == "FUTEX_WAIT_PRIVATE":
+                if line.split()[5] == "NULL":
+                    untimed_wait += 1
+                else:
+                    timed_wait += 1
+
         if 'resumed' in line:
             try:
                 call = line.split()[3]
 
                 if call != "futex":
                     continue
-
-                call_time = line.split(' ')[-1]
-
-                if call_time == "?\n":
-                    continue
-                else:
-                    call_time = float(call_time[1:-2])
-
-                if call in calls:
-                    newmin = min(call_time, calls[call][2])
-                    newmax = max(call_time, calls[call][3])
-                    calls[call] = ( calls[call][0] + 1, calls[call][1] + call_time, newmin, newmax )
-                else:
-                    calls[call] = ( 1, call_time, call_time, call_time )
-
+                
+                return_count += 1
                 retcode = line.split()[4]
 
             except:
@@ -54,3 +52,5 @@ with open(sys.argv[1]) as stracef:
 ops = dict(sorted(ops.items(), key=lambda item: item[1], reverse=True))
 for op, count in ops.items():
     print(op, count, f"{(count/call_count * 100):.1f}%")
+print("Returned:", return_count)
+print("Timed waits:", timed_wait, "Untimed waits:", untimed_wait)
