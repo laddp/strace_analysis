95a111f8acb6729a75546b0d6011b3c2ff6d0efd
Patrick Ladd <pladd@redhat.com>
2024-10-24 11:32:38 -0400

    Add calls/sec

--- strace_counts.py
+++ strace_counts.py
@@ -1,11 +1,22 @@
 #!/usr/bin/python
 import sys
 from pprint import pprint
+from datetime import datetime
+
+start_time = 0
+end_time = 0
+linenum = 0
 
 calls = {}
 call_count = 0
+
 with open(sys.argv[1]) as stracef:
     for line in stracef:
+        linenum += 1
+        if linenum == 1:
+            start_time = line.split()[1]
+        end_time = line.split()[1]
+
         if 'resumed' not in line and 'killed by' not in line and 'exited with' not in line and '--- SIG' not in line:
             call = line.split()[2].split('(')[0]
             call_count += 1
@@ -17,6 +28,12 @@ with open(sys.argv[1]) as stracef:
                 calls[call] = 1
 
 #pprint(calls)
+start = datetime.fromtimestamp(float(start_time))
+end = datetime.fromtimestamp(float(end_time))
+elapsed_time = end - start
+print("Trace start:", start, "Trace end:", end, "Duration:", elapsed_time, "\n")
+
 calls = dict(sorted(calls.items(), key=lambda item: item[1], reverse=True))
+print("call", "count", "%total", "per_sec")
 for call, count in calls.items():
-    print(call, count, f"{(count/call_count * 100):.1f}%")
+    print(call, count, f"{(count/call_count * 100):.1f}%", f"{(count/elapsed_time.seconds):.2f}")
